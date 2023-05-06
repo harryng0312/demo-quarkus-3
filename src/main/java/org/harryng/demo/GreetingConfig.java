891ba3d2eb0cc5cd5846448e2c933a9ea9ef70f5
harrynguyen0312 <Hiepxie1987>
2023-05-06 22:37:37 +0700

    Initial commit

---
+++ src/main/java/org/harryng/demo/GreetingConfig.java
@@ -0,0 +1,12 @@
+package org.harryng.demo;
+
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithName;
+
+@ConfigMapping(prefix = "greeting")
+public interface GreetingConfig {
+
+    @WithName("message")
+    String message();
+
+}
