5758cc94ab4de8247a4fa6709b976b6a8c3786f9
harrynguyen0312 <Hiepxie1987>
2023-05-10 02:00:44 +0700

    add infinispan.xml

--- setup/infinispan/start-cache-2.sh
+++ setup/infinispan/start-cache-2.sh
@@ -1,6 +1,8 @@
 # /bin/sh
 #./server.sh -g "test-infinispan-group" -j "pmobile3-stack" -b "0.0.0.0" -Djgroups.tcpping.initial_hosts=localhost[7800],127.0.0.1[7800]
-./bin/server.sh -s ./server2 -o 100 -b 0.0.0.0 -g "test-infinispan-group" -j "pmobile3-stack" \
+./bin/server.sh -s ./server2 -o 100 -b 127.0.0.1 -g "test-infinispan-group" -j "pmobile3-stack" \
   -Dinfinispan.site.name=site2 \
   -Djgroups.mcast_port=46656 \
-  -Djgroups.tcpping.initial_hosts=localhost[7800],127.0.0.1[7900]
+  -Djgroups.join_timeout=2000 \
+  -Djgroups.tcpping.initial_hosts="localhost[7800],127.0.0.1[7900]"
+#  -Djgroups.gossipAddress=127.0.0.1[17800]
