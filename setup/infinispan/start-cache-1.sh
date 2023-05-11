09443ef03b623549b005e8e99d175b9d40892b6e
harrynguyen0312 <Hiepxie1987>
2023-05-11 23:57:05 +0700

    add infinispan.xml

--- setup/infinispan/start-cache-1.sh
+++ setup/infinispan/start-cache-1.sh
@@ -1,8 +1,9 @@
 # /bin/sh
 #./server.sh -g "test-infinispan-group" -j "pmobile3-stack" -b "0.0.0.0" -Djgroups.tcpping.initial_hosts=localhost[7800],127.0.0.1[7800]
-./bin/server.sh -s ./server -o 0 -b 127.0.0.1 -g "test-infinispan-group" -j "pmobile3-stack" \
-  -Dinfinispan.site.name=site1 \
+export BIND_ADDR=192.168.2.254
+./bin/server.sh -s ./server1 -o 000 -b $BIND_ADDR -g "test-infinispan-group" -j "pmobile3-stack" \
+  -Dinfinispan.site.name=site2 \
   -Djgroups.mcast_port=46656 \
   -Djgroups.join_timeout=2000 \
-  -Djgroups.tcpping.initial_hosts="localhost[7800],127.0.0.1[7900]"
+  -Djgroups.tcpping.initial_hosts="$BIND_ADDR[7800],$BIND_ADDR[7900]"
 #  -Djgroups.gossipAddress=127.0.0.1[17800]
--- setup/infinispan/start-cache-3.sh
+++ setup/infinispan/start-cache-3.sh
@@ -1,7 +1,7 @@
 # /bin/sh
 #./server.sh -g "test-infinispan-group" -j "pmobile3-stack" -b "0.0.0.0" -Djgroups.tcpping.initial_hosts=localhost[7800],127.0.0.1[7800]
 export BIND_ADDR=192.168.2.254
-./bin/server.sh -s ./server2 -o 050 -b $BIND_ADDR -g "test-infinispan-group" -j "pmobile3-stack" \
+./bin/server.sh -s ./server3 -o 100 -b $BIND_ADDR -g "test-infinispan-group" -j "pmobile3-stack" \
   -Dinfinispan.site.name=site2 \
   -Djgroups.mcast_port=46656 \
   -Djgroups.join_timeout=2000 \
