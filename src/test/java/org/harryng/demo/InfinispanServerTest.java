afa034d9a1aa2870aa6c1e5e39db933a3be12acb
harryng0312 <little.ruan@gmail.com>
2023-05-18 17:07:16 +0700

    test get/put to infinispan

---
+++ src/test/java/org/harryng/demo/InfinispanServerTest.java
@@ -0,0 +1,28 @@
+package org.harryng.demo;
+
+import io.quarkus.infinispan.client.Remote;
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
+import org.infinispan.client.hotrod.RemoteCache;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.TimeUnit;
+
+@QuarkusTest
+public class InfinispanServerTest {
+    static Logger logger = LoggerFactory.getLogger(InfinispanServerTest.class);
+    @Inject
+    @Remote("distCache")
+    RemoteCache<String, String> distCache;
+    @Test
+    public void testReadWriteCache() throws InterruptedException {
+        logger.info("hello");
+        distCache.put("test key", "test value at: " + LocalDateTime.now(), 5, TimeUnit.MINUTES);
+        Thread.sleep(1_000);
+        String value = distCache.get("test key");
+        logger.info("get: {}, {}", "test key", value);
+    }
+}
