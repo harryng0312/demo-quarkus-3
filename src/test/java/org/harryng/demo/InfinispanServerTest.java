b28afd66189b3ba0afbb77cee9f8377091c1c98b
harryng0312 <little.ruan@gmail.com>
2023-05-19 16:47:26 +0700

    test get/put to infinispan

--- src/test/java/org/harryng/demo/InfinispanServerTest.java
+++ src/test/java/org/harryng/demo/InfinispanServerTest.java
@@ -7,11 +7,9 @@ import org.infinispan.client.hotrod.DefaultTemplate;
 import org.infinispan.client.hotrod.RemoteCache;
 import org.infinispan.client.hotrod.RemoteCacheManager;
 import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
-import org.infinispan.client.hotrod.configuration.ClientIntelligence;
 import org.infinispan.client.hotrod.impl.ConfigurationProperties;
 import org.infinispan.commons.configuration.BasicConfiguration;
 import org.infinispan.commons.configuration.StringConfiguration;
-import org.infinispan.commons.configuration.XMLStringConfiguration;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -37,8 +35,21 @@ public class InfinispanServerTest {
 
     @Test
     public void testCreateCache() throws InterruptedException {
+        final String configStr = "<?xml version=\"1.0\"?>\n" +
+                "<distributed-cache name=\"distCache\" owners=\"2\" mode=\"SYNC\" statistics=\"true\">\n" +
+                "\t<encoding media-type=\"application/x-protostream\"/>\n" +
+                "\t<locking isolation=\"READ_COMMITTED\"/>\n" +
+                "\t<transaction mode=\"NON_XA\" locking=\"PESSIMISTIC\"/>\n" +
+                "\t<memory storage=\"HEAP\"/>\n" +
+                "\t<persistence passivation=\"true\">\n" +
+                "\t\t<file-store>\n" +
+                "\t\t\t<data path=\"./cache\"/>\n" +
+                "\t\t\t<index path=\"./index\"/>\n" +
+                "\t\t</file-store>\n" +
+                "\t</persistence>\n" +
+                "</distributed-cache>";
         ConfigurationBuilder builder = new ConfigurationBuilder();
-        builder.addCluster("test-local-cluster")
+        builder//.addCluster("test-local-cluster")
                 .addServer().host("192.168.1.13").port(ConfigurationProperties.DEFAULT_HOTROD_PORT)
                 .addServer().host("192.168.1.13").port(ConfigurationProperties.DEFAULT_HOTROD_PORT + 50)
                 .addServer().host("192.168.1.13").port(ConfigurationProperties.DEFAULT_HOTROD_PORT + 100)
@@ -48,8 +59,8 @@ public class InfinispanServerTest {
 //                .realm("default")
 //                .saslMechanism("SCRAM-SHA-512");
         final RemoteCacheManager cacheManager = new RemoteCacheManager(builder.build());
-        BasicConfiguration basicConfiguration = new StringConfiguration("");
-        final RemoteCache<String, String> cache = cacheManager.administration().getOrCreateCache("testCache", DefaultTemplate.DIST_ASYNC);
+        BasicConfiguration basicConfiguration = new StringConfiguration(configStr);
+        final RemoteCache<String, String> cache = cacheManager.administration().getOrCreateCache("testCache", basicConfiguration);
 //        RemoteCache<String, String> cache = cacheManager.getCache("testCache");
         cache.put("test key", "test value at: " + LocalDateTime.now(), 5, TimeUnit.MINUTES);
         Thread.sleep(1_000);
